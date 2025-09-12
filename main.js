9f221a8e8b3e030e01a63f528caf8deba9241c55
ぱむるん <kyoukyou242@gmail.com>
2025-09-12 18:59:51 +0900

    Add files via upload

--- main.js
+++ main.js
@@ -7,17 +7,12 @@ $(function() {
 
 $(window).on('load', function () {
   $('#loader').fadeOut(200, function () {
-    $('#main-content').fadeIn(200);
-  });
-});
-
-$(function () {
-  $('#main-content').fadeIn(200, function () {
-    AOS.init({
-      once: true,
-      startEvent: 'load',
-      duration: 600,
-      easing: 'ease-in-out',
+    $('#main-content').fadeIn(200, function () {
+      AOS.init({
+        once: true,
+        duration: 600,
+        easing: 'ease-in-out',
+      });
     });
   });
 });
